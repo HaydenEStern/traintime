4bed1ef24af589a5d07b18fe9208b3383a19aa43
Hayden Stern <haydenstern@Haydens-MBP.att.net>
2017-10-03 20:59:53 -0700

    style and complete logic

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # traintime
+# HW - traintime_week_7_hw
+
+
+## Live Link (If relevant)
+ - http://haydenestern.github.io/traintime/
+
+## use firebase to generate a train schedule based on user input, visible from multiple browsers
+
+## Requirements
+- Store train schedules in Firebase
+- Base new trains on user input
+- Display schedule in dynamically generated table
+
+## Technologies Used
+- Jquery for Dom Manipulation
+- moment.js for time conversion
+- Firebase for real time database
+- d3 for background gradient
+
+## Code Explaination
+- Use jQuery to store user input and dynamically generate a table
+- use data snapshots every time a new entry is added to firebase
+- use moment to perform time based calculations
+- use d3 to build a background gradient cycler
+
+
+
+
---
+++ app.js
@@ -0,0 +1,109 @@
+  var config = {
+      apiKey: "AIzaSyCQbwT7D8fw3btfgCBsxcgp43Jvocxc5j0",
+      authDomain: "sample-project-64b6d.firebaseapp.com",
+      databaseURL: "https://sample-project-64b6d.firebaseio.com",
+      projectId: "sample-project-64b6d",
+      storageBucket: "sample-project-64b6d.appspot.com",
+      messagingSenderId: "927887821971"
+  };
+  firebase.initializeApp(config);
+  database = firebase.database();
+  $(document).ready(function() {
+
+  	gradient();
+
+  	function gradient() {
+  	d3.select('body')
+  	.transition()
+  	.style("background-color", 'purple')
+  	.duration(10000)
+  	.each("end", function(){
+  		d3.select(this)
+  		.transition()
+  		.style("background-color", 'orange')
+  		.duration(10000)
+  		.each("end", function(){
+  		d3.select(this)
+  		.transition()
+  		.style("background-color", 'pink')
+  		.duration(10000)
+  		.each("end", function(){
+  		gradient();
+  		
+  	});
+  		
+  	});
+
+  	});
+  }
+
+
+  // button for adding train
+  $("#add-train").on("click", function() {
+      event.preventDefault();
+
+      // grabs user input
+      var trainName = $("#name").val().trim();
+      var trainDestination = $("#destination").val().trim();
+      var trainStart = moment($("#firsttrain").val().trim(), "HH:mm").format("X");
+      var trainFrequency = $("#frequency").val().trim();
+      //creates local "temporary object" for holding train data
+      var newTrain = {
+          name: trainName,
+          destination: trainDestination,
+          start: trainStart,
+          frequency: trainFrequency
+      };
+      // uplaods train data to database (push to root)
+      database.ref().push(newTrain);
+      // logs everything to console
+      console.log(newTrain);
+
+
+      // clear out html values in text boxes
+      $("#name").val("");
+      $("#destination").val("");
+      $("#firsttrain").val("");
+      $("#frequency").val("");
+
+  });
+  // create firebase event for adding train to database and a row in HTML table
+  // use event listener with child_added
+
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+      console.log(childSnapshot.val());
+
+      // store everything into a variable
+      var trainName = childSnapshot.val().name;
+      var trainDestination = childSnapshot.val().destination;
+      var trainFrequency = childSnapshot.val().frequency;
+      var trainStart = childSnapshot.val().start;
+
+      var trainStartConverted = moment(trainStart, "hh:mm").subtract(1, "years");
+      console.log(trainStartConverted);
+      // Current Time
+      var currentTime = moment();
+      console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+
+      var timeDiff = moment().diff(trainStartConverted);
+      console.log("Time Diff: " + timeDiff);
+
+      var timeRemainder = timeDiff % trainFrequency;
+
+      var minutesAway = trainFrequency - timeRemainder;
+      console.log(minutesAway);
+
+      // Next Train
+      var nextTrain = moment().add(minutesAway, "minutes");
+      console.log("arrival time: " + moment(nextTrain).format("hh:mm"));
+
+      var arrivalTime = moment(nextTrain).format("hh:mm");
+      // add each part to table
+
+      $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" +
+          trainDestination + "</td><td>" + trainFrequency + "</td><td>" + arrivalTime +
+          "</td><td>" + minutesAway + "</td></tr>");
+
+  });
+
+  	   });
